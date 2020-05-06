458a830e9a29ec52768c5cec09dda41114e1c1a0
Matt <mgmcintyre777@gmail.com>
2020-05-06 05:52:09 -0400

    should work

--- assets/app.js
+++ assets/app.js
@@ -1,71 +1,101 @@
 "use strict";
 
-const $ul_pagesNav = $("#pages-nav");
-const $div_bookArea = $("#book-area");
-
-const numBooks = 20;
 const maxPerPage = 6;
-const numPages = Math.ceil(numBooks/maxPerPage);
-
-const books = [...Array(numBooks).keys()].map((i) => ({
-  bookId: i,
-  title: "Book Title ${i}",
-  author: "Author Name ${i}",
+const fakeBooks = [...Array(20).keys()].map((i) => ({
+  id: i + 1,
+  title: "Book Title " + (i + 1),
+  author: "Author Name " + (i + 1),
 }));
 
-$ul_pagesNav.append(generateNav(books));
+let currentPage = 0;
+$("#pages-nav").append(generateNav(fakeBooks, maxPerPage));
+updateDisplay();
 
-function generateNav(books){
-
-    let navButtons = [];
-
-    console.log(numPages);
-
-    if(numPages>1){
-        let $previous = $("<li>").addClass("page-item").addClass("disabled");        
-        let $previousLink = $("<a>")
-        .addClass("page-link")
-        .html("Previous")
-        .attr("href", "javascript:void(0)")
-        .click(onClick_Previous);
-        $previous.append($previousLink);
-        navButtons.push($previous);
-
-        let $next = $("<li>").addClass("page-item");
-        let $nextLink = $("<a>")
-        .addClass("page-link")
-        .html("Next")
-        .attr("href", "javascript:void(0)")
-        .click(onClick_Next);
-        $next.append($nextLink);
-        navButtons.push($next);
-
-        let pageLinks = [...Array(numPages).keys()].map(page => {
-            let $page = $("<li>").addClass("page-item");
-            let $pageLink = $("<a>")
-            .addClass("page-link")
-            .html(page+1)
-            .attr("href", "javascript:void(0)")
-            .attr("page", page)
-            .click(onClick_Page);
-            $page.append($pageLink);
-            return $page;            
-        });
-        pageLinks[0].addClass("active");
-        navButtons.splice(1,0,...pageLinks);
-    }
-    return navButtons;
+function updateDisplay() {
+  $("#book-area").empty().append(showBooks(fakeBooks, currentPage, maxPerPage));
 }
 
-function onClick_Page(){
-    
-    console.log("click page");
-};
+function onClick_Page() {
+  currentPage = parseInt($(this).attr("page"));
+  updateDisplay(currentPage);
+}
 
-function onClick_Next(){
-    console.log("click previous");
-};
+function onClick_Next() {
+  if (currentPage < Math.ceil(fakeBooks.length / maxPerPage)-1) {
+    currentPage++;
+    updateDisplay();
+  }
+}
 
-function onClick_Previous(){
-    console.log("click next");
-};
+function onClick_Previous() {
+  if (currentPage > 0) {
+    currentPage--;
+    updateDisplay();
+  }
+}
+
+function generateNav(books, maxPerPage) {
+  let navButtons = [];
+  let numPages = Math.ceil(books.length / maxPerPage);
+
+  if (numPages > 1) {
+    let $previous = $("<li>").addClass("page-item");
+    let $previousLink = $("<a>")
+      .addClass("page-link")
+      .html("Previous")
+      .attr("href", "javascript:void(0)")
+      .click(onClick_Previous);
+    $previous.append($previousLink);
+    navButtons.push($previous);
+
+    let $next = $("<li>").addClass("page-item");
+    let $nextLink = $("<a>")
+      .addClass("page-link")
+      .html("Next")
+      .attr("href", "javascript:void(0)")
+      .click(onClick_Next);
+    $next.append($nextLink);
+    navButtons.push($next);
+
+    let pageLinks = [...Array(numPages).keys()].map((page) => {
+      let $page = $("<li>").addClass("page-item");
+      let $pageLink = $("<a>")
+        .addClass("page-link")
+        .html(page + 1)
+        .attr("href", "javascript:void(0)")
+        .attr("page", page)
+        .click(onClick_Page);
+      $page.append($pageLink);
+      return $page;
+    });
+
+    navButtons.splice(1, 0, ...pageLinks);
+  }
+  return navButtons;
+}
+
+function showBooks(books, page, maxPerPage) {
+  let minIndex = maxPerPage * page;
+  let maxIndex = minIndex + maxPerPage;
+  return books
+    .filter((b, i) => i >= minIndex && i < maxIndex)
+    .map((book) => {
+      let $cardDiv = $("<div>").addClass("card");
+      let bookInfo =
+        "Id: ${book.id}, Title: ${book.title}, Author: ${book.author}";
+      console.log(bookInfo);
+      console.log(book.title);
+      let $cardBody = $("<div>")
+        .addClass("card-body")
+        .html(
+          "Id: " +
+            book.id +
+            ", Title: " +
+            book.title +
+            ", Author: " +
+            book.author
+        );
+      $cardDiv.append($cardBody);
+      return $cardDiv;
+    });
+}
